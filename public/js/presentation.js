6d085842f2bcef0d499eccf47e61ac2ae1056a52
Jeff Kunkle <jeff.kunkle@nearinfinity.com>
2012-03-05 09:38:00 -0500

    Add more slides

--- public/js/presentation.js
+++ public/js/presentation.js
@@ -1,10 +1,12 @@
-var socket = io.connect('http://localhost:3000');
+if (window.io) {
+  var socket = io.connect('http://192.168.13.124:3000');
 
-socket.on('step', function (stepId) {
-	var el = document.getElementById(stepId);
-	presentation.goto(el);
-});
+  socket.on('step', function (stepId) {
+    var el = document.getElementById(stepId);
+    presentation.goto(el);
+  });
 
-presentation.on('step', function(step) {
-	socket.emit('step', step.el.id);
-});
+  presentation.on('step', function(step) {
+    socket.emit('step', step.el.id);
+  });
+}
