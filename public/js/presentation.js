bc6480b35dc97c8afa8328434afc1f967cf182b0
Jeff Kunkle <jeff.kunkle@nearinfinity.com>
2012-03-07 10:39:00 -0500

    Add participant counting

--- public/js/presentation.js
+++ public/js/presentation.js
@@ -1,12 +1,17 @@
 if (window.io) {
-  var socket = io.connect('http://192.168.1.52:3000');
+  var socket = io.connect('http://192.168.13.124:3000');
 
   socket.on('step', function (stepId) {
     var el = document.getElementById(stepId);
     presentation.goto(el);
   });
 
-  presentation.on('step', function(step) {
+  presentation.on('step', function (step) {
     socket.emit('step', step.el.id);
+  });
+
+  socket.on('connections', function (num) {
+    var el = document.getElementById('connections');
+    el.innerHTML = num + ' participants';
   });
 }
--- server.js
+++ server.js
@@ -12,50 +12,60 @@ app.configure(function() {
 
 app.listen(3000);
 
+var broadcast = function(event, data, ignoreSocket) {
+  var socketLength = sockets.length;
+  for (var i = 0; i < socketLength; i++) {
+    if (sockets[i] !== ignoreSocket) {
+      sockets[i].emit(event, data);
+    }
+  }
+  if (ignoreSocket === undefined || ignoreSocket !== masterSocket) {
+    masterSocket.emit(event, data);
+  }
+};
+
 var addSocket = function(socket) {
-	if (masterSocket === null) {
-		masterSocket = socket;
-	} else {
-		sockets.push(socket);	
-	}
+  if (masterSocket === null) {
+    masterSocket = socket;
+  } else {
+    sockets.push(socket);
+  }
 };
 
 var removeSocket = function (socket) {
-	if (socket === masterSocket) {
-		masterSocket = null;
-	} else {
-		var socketLength = sockets.length;
-		for (var i = 0; i < socketLength; i++) {
-			if (socket === sockets[i]) {
-				return sockets.splice(i, 1);
-			}
-		}	
-	}
+  if (socket === masterSocket) {
+    masterSocket = null;
+  } else {
+    var socketLength = sockets.length;
+    for (var i = 0; i < socketLength; i++) {
+      if (socket === sockets[i]) {
+        return sockets.splice(i, 1);
+      }
+    }
+  }
 };
 
 var findLocalAddress = function() {
-	var en1 = os.networkInterfaces().en1;
-	for (var i = 0, length = en1.length; i < length; i++) {
-		if (en1[i].family === 'IPv4') {
-			return en1[i].address;
-		}
-	}
+  var en1 = os.networkInterfaces().en1;
+  for (var i = 0, length = en1.length; i < length; i++) {
+    if (en1[i].family === 'IPv4') {
+      return en1[i].address;
+    }
+  }
 }
 
 io.sockets.on('connection', function (socket) {
-	addSocket(socket);
-  	socket.on('step', function (stepId) {
-		if (socket === masterSocket) {
-			var socketLength = sockets.length;
-			for (var i = 0; i < socketLength; i++) {
-				sockets[i].emit('step', stepId);
-			}
-		}
-  	});
-  	socket.on('disconnect', function() {
-		console.log('Socket disconnected');
-		removeSocket(socket);
-	})
+  addSocket(socket);
+  socket.on('step', function (stepId) {
+    if (socket === masterSocket) {
+      broadcast('step', stepId, masterSocket);
+    }
+  });
+  socket.on('disconnect', function() {
+    console.log('Socket disconnected');
+    removeSocket(socket);
+  });
+  broadcast('connections', sockets.length + 1);
 });
 
 console.log('Connect to http://' + findLocalAddress() + ':3000');
